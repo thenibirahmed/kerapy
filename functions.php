36e15902d1bacdd97ec3e1a6e4094a5f1cf476fc
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-10-12 23:29:40 +0600

    Carbon fields installed

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php 
 
 function kerapy_basic_functions(){
+    require_once( 'vendor/autoload.php' );
+    require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+
     add_theme_support('title-tag');
     add_theme_support('menus');
     add_theme_support('post-thumbnails');
@@ -10,11 +13,6 @@ function kerapy_basic_functions(){
     add_theme_support('post-formats', array('audio','gallery','video'));
     add_theme_support('woocommerce');
 
-    if ( ! file_exists( get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php' ) ) {
-        return new WP_Error( 'class-wp-bootstrap-navwalker-missing', 'It appears the class-wp-bootstrap-navwalker.php file may be missing.' );
-    } else {
-        require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
-    }
 
     register_nav_menus( array(
         'primary' => __( 'Primary Menu', 'THEMENAME' ),
@@ -59,17 +57,7 @@ function kerapy_basic_functions(){
         'before_title'  => '<h5 class="footer-list-header">',
         'after_title'   => '</h5>',
     ) );
+
+    \Carbon_Fields\Carbon_Fields::boot();
 }
 add_action('after_setup_theme', 'kerapy_basic_functions');
-
-
-// function enqueue_theme_assets(){
-//     // Styles
-//     wp_enqueue_style( 'normal', get_template_directory_uri() . '/css/normalize.css', array(), '1.0' );
-//     wp_enqueue_style( 'style', get_stylesheet_uri());
-
-
-//     // Scripts
-//     wp_enqueue_script( 'imagesload', get_template_directory_uri() . '/js/imagesloaded.pkgd.min.js', array(),'1.0', true );
-// }
-// add_action( 'wp_enqueue_scripts', 'enqueue_theme_assets' );
