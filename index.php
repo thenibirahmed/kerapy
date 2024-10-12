4fca8b891f0249c9a0c82a86dd0fd7aa6dd3e167
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-10-12 22:43:24 +0600

    menu works

--- index.php
+++ index.php
@@ -25,16 +25,16 @@
                     <span class="navbar-toggler-icon"></span>
                     </button>
 					<?php 
-					wp_nav_menu( array(
-						'theme_location'  => 'primary',
-						'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-						'container'       => 'div',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'bs-example-navbar-collapse-1',
-						'menu_class'      => 'navbar-nav mx-auto mb-2 mb-lg-0 gap-md-4',
-						'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-						'walker'          => new WP_Bootstrap_Navwalker(),
-					) );
+						wp_nav_menu( array(
+							'theme_location'  => 'primary',
+							'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+							'container'       => 'div',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'bs-example-navbar-collapse-1',
+							'menu_class'      => 'navbar-nav mx-auto mb-2 mb-lg-0 gap-md-4',
+							'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+							'walker'          => new WP_Bootstrap_Navwalker(),
+						) );
 					?>
                     <div>
 						<a href="#" class="btn btn-outline-dark">Contact Us</a>
