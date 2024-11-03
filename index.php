4d03794229ca57b361e72118de9e0d20d798fb61
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-11-03 12:12:05 +0600

    post format added

--- index.php
+++ index.php
@@ -31,19 +31,7 @@
 		</div>
 		<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5">
 			<?php while(have_posts()) : the_post() ?>
-				<div class="col">
-					<a href="<?php the_permalink() ?>" class="text-decoration-none">
-						<div class="card h-100 overflow-hidden rounded-0 border-0">
-							<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="Manual Therapy" class="card-img-top rounded-0 img-fluid">
-							<div class="card-body pt-3 p-0">
-								<h6 class="card-title"><?php the_title() ?></h6>
-								<h6 class="sub-head text-black-50 d-flex">
-									<?php the_category() ?> <span class="mx-2">|</span> <?php echo get_the_date('M d, Y') ?>
-								</h6>
-							</div>
-						</div>
-					</a>
-				</div>
+				<?php get_template_part('template-parts/content', get_post_format()); ?>
 			<?php endwhile ?>
 
 			<?php the_posts_pagination() ?>
---
+++ template-parts/content.php
@@ -0,0 +1,13 @@
+<div class="col">
+    <a href="<?php the_permalink()?>" class="text-decoration-none">
+        <div class="card h-100 overflow-hidden rounded-0 border-0">
+            <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="Manual Therapy" class="card-img-top rounded-0 img-fluid">
+            <div class="card-body pt-3 p-0">
+                <h6 class="card-title"><?php the_title()?></h6>
+                <h6 class="sub-head text-black-50 d-flex">
+                    <?php the_category()?> <span class="mx-2">|</span> <?php echo get_the_date( 'M d, Y' ) ?>
+                </h6>
+            </div>
+        </div>
+    </a>
+</div>
