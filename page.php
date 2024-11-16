d34e2757a6c8d1ddd428ff91d80eff8a9edf3162
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-11-16 17:42:50 +0600

    container issue solved

--- page.php
+++ page.php
@@ -1,5 +1,23 @@
-<?php get_header()?>
-    <?php while ( have_posts() ): the_post()?>
-        <?php the_content()?>
-    <?php endwhile ?>
-<?php get_footer()?>
+<?php 
+    get_header();
+
+    if ( \Elementor\Plugin::$instance->documents->get( get_the_ID() )->is_built_with_elementor() ) {
+        while ( have_posts() ): 
+            the_post();
+            the_content();
+        endwhile;
+    } else {
+        ?>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1><?php the_title() ?></h1>
+                        <?php the_content() ?>
+                    </div>
+                </div>
+            </div>
+        <?php
+    }
+
+    get_footer();
+?>
