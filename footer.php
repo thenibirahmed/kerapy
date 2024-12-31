2007f607e22d23a1c5f65c9ad68dea70ddceab21
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-12-31 09:38:26 +0600

    custom header footer system added

--- footer.php
+++ footer.php
@@ -1,48 +1,64 @@
 <?php 
-global $kerapy_option;
-$footer_right_text = $kerapy_option['footer_right_copyright'];
-$footer_menu = $kerapy_option['footer_menu'];
+    global $kerapy_option;
+    $footer_right_text = $kerapy_option['footer_right_copyright'];
+    $footer_menu = $kerapy_option['footer_menu'];
+    $headerTemplate = $kerapy_option['kerapy-footer-template-select']; 
+    if($headerTemplate == 'default'):
 ?>   
-<footer class="text-white pt-5 footer-bg-color">
-    <div class="container py-md-4">
-        <div class="row">
-            <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-                <?php dynamic_sidebar('kerapy-footer-1') ?>
+    <footer class="text-white pt-5 footer-bg-color">
+        <div class="container py-md-4">
+            <div class="row">
+                <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+                    <?php dynamic_sidebar('kerapy-footer-1') ?>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+                    <?php dynamic_sidebar('kerapy-footer-2') ?>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+                    <?php dynamic_sidebar('kerapy-footer-3') ?>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+                    <?php dynamic_sidebar('kerapy-footer-4') ?>
+                </div>
             </div>
-            <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-                <?php dynamic_sidebar('kerapy-footer-2') ?>
+            <hr class="bg-light mt-5 mb-4">
+            <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between pb-3 gap-2">
+                <?php 
+                    if($footer_menu){
+                        wp_nav_menu(array(
+                            'menu' => $footer_menu,
+                            'menu_class' => 'd-flex flex-wrap footer-bottom-menu gap-3 mb-0 ',
+                            'before' => '<div class="footer-list-item">'
+                        ));
+                    }
+                ?>
+                <div>
+                    <p class="mb-0 footer-list-item"><?php echo esc_html($footer_right_text); ?></p>
+                </div>
             </div>
-            <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-                <?php dynamic_sidebar('kerapy-footer-3') ?>
-            </div>
-            <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-                <?php dynamic_sidebar('kerapy-footer-4') ?>
-            </div>
+            
         </div>
-        <hr class="bg-light mt-5 mb-4">
-        <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between pb-3 gap-2">
-            <?php 
-                if($footer_menu){
-                    wp_nav_menu(array(
-                        'menu' => $footer_menu,
-                        'menu_class' => 'd-flex flex-wrap footer-bottom-menu gap-3 mb-0 ',
-                        'before' => '<div class="footer-list-item">'
-                    ));
-                }
-            ?>
-            <div>
-                <p class="mb-0 footer-list-item"><?php echo esc_html($footer_right_text); ?></p>
-            </div>
-        </div>
-        
-    </div>
-</footer>
+    </footer>
+<?php else:         
+    $page = get_post($headerTemplate);
+
+    if ($page) {
+        echo apply_filters('the_content', $page->post_content);
+    } else {
+        echo 'Template not found.';
+    }
+
+endif; ?>
+
 <style>
     <?php echo wp_kses_post($kerapy_option['css_on_body_end']); ?>
 </style>
+
 <script>
     <?php echo wp_kses_post($kerapy_option['js_on_body_end']); ?>
 </script>
+
 <?php wp_footer(); ?>
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -28,46 +28,59 @@ $btn_bg_color = $kerapy_option['h-btn-bg'];
         <?php echo wp_kses_post($kerapy_option['js_on_body_start']); ?>
     </script>
 <?php wp_body_open(); ?>
-    <div class="menu">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container py-md-4">
-                <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
-                    <?php 
-                        if(!empty($kerapy_option['kerapy-logo']['url'])){
-                            ?>
-                            <img src="<?php echo esc_url($kerapy_option['kerapy-logo']['url']); ?>" alt="<?php echo bloginfo('name');?>">
-                            <?php
-                        }else{
-                            echo bloginfo('name');
-                        }
-                    ?>
-                </a>
-                <button class="navbar-toggler menu-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
-                    <?php
-                        wp_nav_menu( array(
-                            'theme_location'  => 'primary',
-                            'depth'           => 2,
-                            'container' => 'div',
-                            'menu_class'      => 'navbar-nav mx-auto mb-2 mb-lg-0 gap-md-4',
-                            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                            'walker'          => new WP_Bootstrap_Navwalker(),
-                        ) );
-                    ?>
+
+<?php $headerTemplate = $kerapy_option['kerapy-header-template-select']; 
+    if($headerTemplate == 'default'):
+?>
+<div class="menu">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container py-md-4">
+            <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
                 <?php 
-                if(!empty($btn_text)){
-                    ?>
-                    <div class="header-btn">
-                        <a href="<?php echo esc_url($btn_url); ?>" class="btn btn-outline-dark " >
-                            <?php echo esc_html($btn_text);?>
-                        </a>
-                     </div>
-                    <?php
-                }?>
+                    if(!empty($kerapy_option['kerapy-logo']['url'])){
+                        ?>
+                        <img src="<?php echo esc_url($kerapy_option['kerapy-logo']['url']); ?>" alt="<?php echo bloginfo('name');?>">
+                        <?php
+                    }else{
+                        echo bloginfo('name');
+                    }
+                ?>
+            </a>
+            <button class="navbar-toggler menu-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
+                <?php
+                    wp_nav_menu( array(
+                        'theme_location'  => 'primary',
+                        'depth'           => 2,
+                        'container' => 'div',
+                        'menu_class'      => 'navbar-nav mx-auto mb-2 mb-lg-0 gap-md-4',
+                        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker'          => new WP_Bootstrap_Navwalker(),
+                    ) );
+                ?>
+            <?php 
+            if(!empty($btn_text)){
+                ?>
+                <div class="header-btn">
+                    <a href="<?php echo esc_url($btn_url); ?>" class="btn btn-outline-dark " >
+                        <?php echo esc_html($btn_text);?>
+                    </a>
                 </div>
-                
+                <?php
+            }?>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
+</div>
+<?php else:         
+    $page = get_post($headerTemplate);
+
+    if ($page) {
+        echo apply_filters('the_content', $page->post_content);
+    } else {
+        echo 'Template not found.';
+    }
+ endif; ?>
+    
