fcf3037ac69784d46d43e69b9af44d0a3160568e
showebb <showeb.eic@gmail.com>
2025-02-12 12:16:56 +0600

    single page updated

---
+++ single-service.php
@@ -0,0 +1,22 @@
+<?php 
+    get_header();
+
+    if ( \Elementor\Plugin::$instance->documents->get( get_the_ID() )->is_built_with_elementor() ) {
+        while ( have_posts() ): 
+            the_post();
+            the_content();
+        endwhile;
+    } else {
+        ?>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php the_content() ?>
+                    </div>
+                </div>
+            </div>
+        <?php
+    }
+
+    get_footer();
+?>
